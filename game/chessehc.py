387abf841f8ea09e5adb5ea7155f9a0405ffc558
DragonMoffon <happydog.aja@gmail.com>
2026-08-08 16:08:44 +1200

    highlight mirrored tiles

--- game/chessehc.py
+++ game/chessehc.py
@@ -29,14 +29,23 @@ class Board:
 
         if 0 <= self.mirror_slice and x < self.mirror_slice:
             x = (2 * self.mirror_slice) - 1 - x
-
-        if self.mirror_slice <= 0 and self.width + self.mirror_slice <= x:
+        elif self.mirror_slice <= 0 and self.width + self.mirror_slice <= x:
             x = (2 * (self.width + self.mirror_slice)) - 1 - x
 
         if not (0 <= x < self.width) or not (0 <= y < self.height):
             return None
 
         return self.grid[x][y]
+
+    def location_mirrored(self, location: tuple[int, int]):
+        x, _ = location
+        if self.mirror_slice is None:
+            return False
+
+        return bool(
+            (0 <= self.mirror_slice and x < self.mirror_slice)
+            or (self.mirror_slice <= 0 and self.width + self.mirror_slice <= x)
+        )
 
     def __str__(self):
         lines = (", ".join(str(self[x, y]) for x in range(self.width)) for y in range(self.height))
@@ -47,7 +56,7 @@ class ChessehcView(View):
     def __init__(self):
         super().__init__()
         self.board = Board(8, 8)
-        self.board.mirror_slice = -2
+        self.board.mirror_slice = 2
 
         self.camera = Camera2D()
         self.camera.position = (
@@ -69,7 +78,10 @@ class ChessehcView(View):
             )
             for x in range(self.board.width):
                 for y in range(self.board.height):
-                    color = (255, 255, 255) if (x + y) % 2 == 0 else (0, 0, 0)
+                    if self.board.location_mirrored((x, y)):
+                        color = (0, 0, 255)
+                    else:
+                        color = (255, 255, 255) if (x + y) % 2 == 0 else (0, 0, 0)
                     draw_lbwh_rectangle_filled(
                         x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, color
                     )
